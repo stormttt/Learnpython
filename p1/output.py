96288b8cabfb121080bb36765d2e6b602697a90a
tong <wangtong@WangTongs-MacBook-Pro.local>
2022-06-18 00:49:54 +0800

    new thing

---
+++ p1/output.py
@@ -0,0 +1,18 @@
+# practice output: 
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa!'''
+
+print('n = 123')
+print('f = 456.789')
+print('s1 = \'Hello, world\'')
+print("s2 = 'Hello, \\\'Adam\\\'\'")
+print("s3 = r\'Hello, \"Bart\"\'")
+print("s4 = r\'\'\'Hello,")
+print("Lisa!\'\'\'")
+
+print('n = 123 \nf = 456.789')
