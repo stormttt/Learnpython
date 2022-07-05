86862c05c715d1aad2bbba8bbd48d0f705ab0df1
stromttt <xyxiaoYU1992@gmail.com>
2022-07-05 20:00:06 +0800

    newthing

--- p3/mapreduce.py
+++ p3/mapreduce.py
@@ -2,12 +2,12 @@
 # 一个函数就可以接收另一个函数作为参数，这种函数就称之为高阶函数
 # map()函数接收两个参数，一个是函数，一个是Iterable，
 # map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回
-L = []
-for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-    L.append(f(n))
-print(L)
+def f(x):
+    return x*x
 
-L = []
-for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-    L.append(f(n))
-print(L)
+l = map(f,[1,2,3,4])
+# pyhton3 中map返回一个list的object 不能直接输出，python2中直接返回list
+print(list(l))
+
+l1=[1,2,3,4]
+print(l1)
