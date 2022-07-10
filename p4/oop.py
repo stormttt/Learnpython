c4d5713e90dfc542f7d4c376237bd5b17f60b3d5
stromttt <xyxiaoYU1992@gmail.com>
2022-07-10 23:41:36 +0800

    finish

--- p4/oop.py
+++ p4/oop.py
@@ -1,2 +1,14 @@
 'Object Oriented Programming'
-print('hellp')
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+
+bart = Student('bart',99)
+lisa = Student('lisa',98)
+
+bart.print_score()
