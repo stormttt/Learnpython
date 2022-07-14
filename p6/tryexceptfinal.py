9b01c56d0f42b96730b8423e293e0a29a8aa0731
stromttt <xyxiaoYU1992@gmail.com>
2022-07-15 03:31:20 +0800

    test

---
+++ p6/tryexceptfinal.py
@@ -0,0 +1,63 @@
+import logging
+
+try:
+    print('try...')
+    r = 10 / 0
+    print('result:', r)
+except ZeroDivisionError as e:
+    print('except:', e)
+finally:
+    print('finally')
+print('end')
+
+try:
+    print('try...')
+    r = 10 / int('a')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+finally:
+    print('finally...')
+print('END')
+# def foo(s):
+#     return 10 / int(s)
+
+# def bar(s):
+#     return foo(s) * 2
+
+# def main():
+#     bar('0')
+
+# main()
+# Python内置的logging模块可以非常容易地记录错误信息
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo('0')
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
